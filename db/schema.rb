cd4f4fa45dc6b7cb21205a56ba5e3fd643a38713
Max Ludvigsson <maxiludvigsson@gmail.com>
2013-03-25 16:01:22 +0100

    Added Year and program to the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130325103611) do
+ActiveRecord::Schema.define(:version => 20130325145731) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
     t.string   "url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "year"
+    t.string   "program"
   end
 
 end
