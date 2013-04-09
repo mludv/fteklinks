f620175ef1fa78e19fbcad7acc6b717f96e43b39
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2013-04-09 09:08:37 +0200

    Show settings first visit

--- public/scripts.js
+++ public/scripts.js
@@ -20,17 +20,42 @@ app.factory('Settings', function(localStorageService) {
     console.log(data);
   };
 
+
   load();
   return {data: data, save:save, load:load};
 
 });
 
 app.controller('SettingsCtrl', function($scope, Settings) {
+  
+  // We want to show the settings menu if the settings is not defined
+  var isFirstTime = false;
+  $scope.checkFirstTime = function() {
+    if (!Settings.data.year || !Settings.data.period || !Settings.data.program) {
+      $('#settings').collapse('show');
+      isFirstTime = true;
+    }
+  };
+
+  // We want to close it when all settings is choosed for the first time.
+  var settingsFinished = function() {
+    if (isFirstTime 
+      && Settings.data.year 
+      && Settings.data.period 
+      && Settings.data.program) {
+      
+        $('#settings').collapse('hide');
+        isFirstTime = false;
+    }
+
+  };
 
   // Sets the settings-object
   $scope.set = function(prop, value) {
+    
     Settings.data[prop] = value;
     Settings.save();
+    settingsFinished()
   };
 
   // Check if equal to current settings
